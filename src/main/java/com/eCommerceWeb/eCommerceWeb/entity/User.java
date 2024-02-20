cc05dac951d972d1ade905f94f92f8143103c768
umayanganacooray <ume.cooray@gmail.com>
2024-02-20 20:59:05 +0530

    creating users table

--- src/main/java/com/eCommerceWeb/eCommerceWeb/entity/User.java
+++ src/main/java/com/eCommerceWeb/eCommerceWeb/entity/User.java
@@ -1,16 +1,23 @@
 package com.eCommerceWeb.eCommerceWeb.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
 public class User {
-
+    @Column
     private int id;
-
+    @Column
     private String email;
-
+    @Column
     private String firstName;
-
+    @Column
     private String lastName;
-
+    @Column
     private String address;
-
+    @Column
     private String password;
 
     public String getEmail() {
