e8be563beea7006295c4b74d86de4856d126c447
SupuniAbeysinghe <supuniab1@gmail.com>
2024-02-20 20:08:52 +0530

    spring init

---
+++ src/main/java/com/eCommerceWeb/eCommerceWeb/ECommerceWebApplication.java
@@ -0,0 +1,13 @@
+package com.eCommerceWeb.eCommerceWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eCommerceWeb/eCommerceWeb/ECommerceWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eCommerceWeb.eCommerceWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
