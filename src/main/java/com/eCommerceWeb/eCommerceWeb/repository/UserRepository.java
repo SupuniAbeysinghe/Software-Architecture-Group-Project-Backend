12832032e45a6c4901373cc589ca8f550a6a9d33
umayanganacooray <ume.cooray@gmail.com>
2024-02-20 20:37:03 +0530

    Adding data UserController Class

--- src/main/java/com/eCommerceWeb/eCommerceWeb/repository/UserRepository.java
+++ src/main/java/com/eCommerceWeb/eCommerceWeb/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.eCommerceWeb.eCommerceWeb.repository;
 
 import com.eCommerceWeb.eCommerceWeb.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
 
