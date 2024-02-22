b8cb60b76d9b607d747c7e62e46be35f1a72e1ce
umayanganacooray <ume.cooray@gmail.com>
2024-02-22 12:48:54 +0530

    Adding missed RestController annotation

--- src/main/java/com/eCommerceWeb/eCommerceWeb/controller/UserController.java
+++ src/main/java/com/eCommerceWeb/eCommerceWeb/controller/UserController.java
@@ -5,6 +5,7 @@ import com.eCommerceWeb.eCommerceWeb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class UserController {
     UserRepository userRepository;
     @Autowired
