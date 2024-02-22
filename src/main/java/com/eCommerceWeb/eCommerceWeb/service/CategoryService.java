81f68bb67be5353933d34fe5123cb72fd679b48b
SupuniAbeysinghe <supuniab1@gmail.com>
2024-02-22 18:09:18 +0530

    create admin conroller and category services

--- src/main/java/com/eCommerceWeb/eCommerceWeb/service/CategoryService.java
+++ src/main/java/com/eCommerceWeb/eCommerceWeb/service/CategoryService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -16,5 +17,12 @@ public class CategoryService {
     }
     public void addCategory(Category category){
         categoryRepository.save(category);
+    }
+    public void removeCategoryById(int id){
+        categoryRepository.deleteById(id);
+    }
+    public Optional<Category> getCategoryById(int id){
+        return categoryRepository.findById(id);
+
     }
 }
