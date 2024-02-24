4967fdfae690bc2a3ae2403eb77bbeb8b63ea0b6
umayanganacooray <ume.cooray@gmail.com>
2024-02-24 23:51:34 +0530

    Add @SpringBootApplication(exclude = SecurityAutoConfiguration.class)

--- src/main/java/com/eCommerceWeb/eCommerceWeb/ECommerceWebApplication.java
+++ src/main/java/com/eCommerceWeb/eCommerceWeb/ECommerceWebApplication.java
@@ -2,8 +2,9 @@ package com.eCommerceWeb.eCommerceWeb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ECommerceWebApplication {
 
 	public static void main(String[] args) {
