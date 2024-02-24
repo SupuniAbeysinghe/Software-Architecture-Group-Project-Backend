9ab9c9c81e4a2711d7381b48636c88601d718b29
umayanganacooray <ume.cooray@gmail.com>
2024-02-24 05:39:58 +0530

    Adding UserDTO.java

---
+++ src/main/java/com/eCommerceWeb/eCommerceWeb/dto/UserDTO.java
@@ -0,0 +1,17 @@
+package com.eCommerceWeb.eCommerceWeb.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDTO {
+    private int id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String password;
+}
