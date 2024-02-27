8b5302f53afad4384104b44cbc5a06dbacc1dc59
NaduniTashana <nadutash0@gmail.com>
2024-02-27 20:07:23 +0530

    update prductRepository

--- src/main/java/com/eCommerceWeb/eCommerceWeb/repository/ProductRepository.java
+++ src/main/java/com/eCommerceWeb/eCommerceWeb/repository/ProductRepository.java
@@ -2,8 +2,11 @@ package com.eCommerceWeb.eCommerceWeb.repository;
 
 import com.eCommerceWeb.eCommerceWeb.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 @Repository
@@ -12,4 +15,10 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     List<Product> findByCategories_Name(String categoryName);
 
+
+
+@Query("SELECT p FROM Product p WHERE lower(p.product_name) LIKE %:key1% OR lower(p.description) LIKE %:key2%")
+public List<Product> findByProductNameOrDescriptionContainingIgnoreCase(@Param("key1") String key1, @Param("key2") String key2);
 }
+
+
