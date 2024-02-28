8ad9cc0b6dc0fb4654bbf5e65fed59442c3ac898
umayanganacooray <ume.cooray@gmail.com>
2024-02-28 19:45:52 +0530

    Created Registration and try to develop  WebSecurityConfig

--- src/main/java/com/eCommerceWeb/eCommerceWeb/entity/User.java
+++ src/main/java/com/eCommerceWeb/eCommerceWeb/entity/User.java
@@ -4,17 +4,23 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Table(name="users")
-public class User {
+public class User implements UserDetails {
     @Id
-    @GeneratedValue (strategy = GenerationType.AUTO)
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private int id;
-    @Column
+    @Column(nullable = false,unique = true)
     private String email;
     @Column
     private String firstName;
@@ -22,24 +28,55 @@ public class User {
     private String lastName;
     @Column
     private String address;
-    @Column
+    @Column(nullable = false)
     private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    private Boolean locked;
+    private Boolean enabled;
 
-    public String getEmail() {
-        return email;
+    public User(int id, String email, String firstName, String lastName, String address, String password) {
+        this.id = id;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.password = password;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
     public String getPassword() {
         return password;
     }
 
-    public String toStringNew() {
-        return "User{" +
-                "email='" + email + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", address='" + address + '\'' +
-                '}';
+    @Override
+    public String getUsername() {
+        return email;
     }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
 
 }
