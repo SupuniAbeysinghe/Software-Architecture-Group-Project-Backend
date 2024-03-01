fc34387389a3422ba1beb1b11242ebb4a8e788a9
NaduniTashana <nadutash0@gmail.com>
2024-03-01 11:45:49 +0530

    add findbyId method

--- src/main/java/com/eCommerceWeb/eCommerceWeb/repository/CategoryRepository.java
+++ src/main/java/com/eCommerceWeb/eCommerceWeb/repository/CategoryRepository.java
@@ -2,5 +2,6 @@ package com.eCommerceWeb.eCommerceWeb.repository;
 import com.eCommerceWeb.eCommerceWeb.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface CategoryRepository extends JpaRepository<Category,Integer>{
+    Category findById(int id);
 
 }
