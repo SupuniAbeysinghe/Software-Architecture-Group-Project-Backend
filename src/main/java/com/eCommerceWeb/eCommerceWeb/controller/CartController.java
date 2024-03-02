fe759dc241183ff52b07063db7404d850c379ab0
SandalikaLiyanage <sandalika24@gmail.com>
2024-03-02 22:59:30 +0530

    retrieves the user's email address, uses it to fetch the corresponding user entity

--- src/main/java/com/eCommerceWeb/eCommerceWeb/controller/CartController.java
+++ src/main/java/com/eCommerceWeb/eCommerceWeb/controller/CartController.java
@@ -1,5 +1,11 @@
 package com.eCommerceWeb.eCommerceWeb.controller;
 
+import com.eCommerceWeb.eCommerceWeb.entity.ShoppingCart;
+import com.eCommerceWeb.eCommerceWeb.entity.User;
+import com.eCommerceWeb.eCommerceWeb.repository.UserRepository;
+import com.eCommerceWeb.eCommerceWeb.service.ShoppingCartService;
+import com.eCommerceWeb.eCommerceWeb.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +14,22 @@ import java.security.Principal;
 
 @Controller
 public class CartController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ShoppingCartService cartService;
 
     @GetMapping("/cart")
     public String cart(Model model, Principal principal){
         if(principal == null){
             return "redirect:/login";
         }
+
+        String email=principal.getName();
+        User user = userRepository.findByEmail(email);
+        ShoppingCart shoppingCart=user.getShoppingCart();
+        model.addAttribute("shoppingCart",shoppingCart);
         return "cart";
     }
 }
